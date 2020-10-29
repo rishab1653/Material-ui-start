8eb2ba34794142a833b6ce0008e8063a7060f067
rishab1653 <rishab959290@gmail.com>
2020-10-29 12:43:21 +0530

    Added Properties of Material-Ui Button

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
-import React from 'react'
+import React,{useState} from 'react'
 import './App.css';
 import {Button} from '@material-ui/core'
 
 function App() {
+
+  const [color , setColor] = useState("primary")
+  const [disabled , setDisabled] =useState(false)
+  const change =() =>{
+    setColor("secondary")
+    alert("Cliked ")
+    setDisabled(true)
+   
+
+  }
   return (
     <div>
     <h1>Hello Material Ui</h1>
     
     <Button
-     color = "primary"
+     color = {color}
      variant="outlined"
+     disabled={disabled}
     //  disabled
-     onClick={()=>{alert("Button Clicked Successfully")}}
+     onClick={()=>{change()}}
      >
     MUI Button
     </Button>
