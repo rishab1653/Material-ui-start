970a54e229f0cdbd9ab2e15db36164a5e5bf5b17
rishab1653 <rishab959290@gmail.com>
2020-10-30 11:43:11 +0530

    Added Button Group Concepts

--- src/App.js
+++ src/App.js
@@ -1,40 +1,54 @@
 import React,{useState} from 'react'
 import './App.css';
+import {ButtonGroup, Button} from '@material-ui/core'
 import {Delete} from '@material-ui/icons'
-import {Button} from '@material-ui/core'
+
 
 
 function App() {
+  return(
+    <div>
+    <h1>Material _UI "Button Group"</h1>
+    <ButtonGroup color="primary" variant="contained" orientation="vertical">
+    <Button>One</Button>
+    <Button>Two</Button>
+    <Button>Three</Button>
+    </ButtonGroup>
+    </div>
+  )
+}
 
-  const [color , setColor] = useState("primary")
-  const [disabled , setDisabled] =useState(false)
-  const change =() =>{
-    setColor("secondary")
-    alert("Cliked ")
-    setDisabled(true)
+  // const [color , setColor] = useState("primary")
+  // const [disabled , setDisabled] =useState(false)
+  // const change =() =>{
+  //   setColor("secondary")
+  //   alert("Cliked ")
+  //   setDisabled(true)
   
    
 
-  }
-  return (
-    <div>
-    <h1>Hello Material Ui</h1>
+ 
+//   //return (
+//     //<div>
+//    // </div
+//   //  > 
+//     <h1>Hello Material Ui</h1>
     
-    <Button
-     color = {color}
-     variant="outlined"
-     disabled={disabled}
-    //  disabled
-     onClick={()=>{change()}}
-    //  startIcon={<Delete />}
-    endIcon = {<Delete/>}
-     >
-    MUI Button
+//     // <Button
+//     //  color = {color}
+//     //  variant="outlined"
+//     //  disabled={disabled}
+//     // //  disabled
+//     //  onClick={()=>{change()}}
+//     // //  startIcon={<Delete />}
+//     // endIcon = {<Delete/>}
+//     //  >
+//     // MUI Button
      
-    </Button>
+//     // </Button>
       
-    </div>
-  );
-}
+//     </div>
+//   );
+// }
 
 export default App;
